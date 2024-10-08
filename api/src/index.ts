31ae8940488242a81900beb602f7b3754a8ff4a4
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-10-08 11:28:32 +0200

    initialisation graphql et appolo

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,30 +1,70 @@
-import express from "express";
-import router from "./router";
-import * as dotenv from "dotenv";
-import cors from "cors";
+// import express from "express";
+// import router from "./router";
+// import * as dotenv from "dotenv";
+// import cors from "cors";
 
-dotenv.config();
+// dotenv.config();
 
-const { PORT } = process.env;
+// const { PORT } = process.env;
 
-import { dataSource } from "./db/client";
-import "reflect-metadata";
+// import { dataSource } from "./db/client";
+// import "reflect-metadata";
 
-const app = express();
+// const app = express();
 
-// app.get ('/maroute', fonction de callback)
+// // app.get ('/maroute', fonction de callback)
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL as string,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL as string,
+//   })
+// );
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use("/api", router);
+// app.use("/api", router);
 
-app.listen(PORT, async () => {
-  await dataSource.initialize();
-  console.log(`Serveur is listenning on http://localhost:${PORT}`);
-});
+// app.listen(PORT, async () => {
+//   await dataSource.initialize();
+//   console.log(`Serveur is listenning on http://localhost:${PORT}`);
+// });
+
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+import repos from "../data/repos.json";
+
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Repo" type defines the queryable fields for every repo in our data source.
+  type Repo {
+    id: String
+    name: String
+    url: String
+    isFavorite: Boolean
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    repos: [Repo]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    repos: () => repos,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+(async () => {
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+
+  console.log(`Server ready at ${url}`);
+})();
