501fe7d6f09b89c21be5cc965e18d91a6c0ead96
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-11-17 16:15:11 +0100

    correction of eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import { configs as tsConfigs } from "@typescript-eslint/eslint-plugin";
+import { parser } from "@typescript-eslint/parser";
 import pluginReact from "eslint-plugin-react";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: parser,
+    },
+    rules: {},
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  tsConfigs.recommended,
   pluginReact.configs.flat.recommended,
 ];
